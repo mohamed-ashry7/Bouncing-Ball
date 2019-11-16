0ce6deb961d39f146b1a60ea0e0e36194c6fabd3
mohamed-ashry7 <mohamed.ashrymahmoud@student.guc.edu.eg>
2019-11-16 19:30:35 +0200

    almost finished

--- OpenGL3DTemplate.cpp
+++ OpenGL3DTemplate.cpp
@@ -1,9 +1,62 @@
-#include <glut.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <windows.h>
+#include <cstdio>
 #include <math.h>
+#include <string.h>
+#include <string>
+#include <glut.h>
 
 #define ROOM_SIZE 10
 #define BRICK_SIZE 2
+#define NUMBER_OF_BRICKS (ROOM_SIZE/BRICK_SIZE)
 
+
+
+class Color {
+private :
+	float r; 
+	float g; 
+	float b; 
+public :
+	Color(float rr, float gg, float bb) {
+		r = rr/255.0; g = gg/255.0; b = bb/255.0;
+	}
+	float getRed() {
+		return r; 
+	}
+	float getBlue() {
+		return b; 
+	}
+	float getGreen() {
+		return g; 
+	}
+
+};
+
+class Wall {
+private:
+	int grid[NUMBER_OF_BRICKS][NUMBER_OF_BRICKS]; 
+public :
+	
+	void setCell(int x , int y , int c ) {
+		grid[x][y] = c; 
+		
+	}
+	int getCellPoints(int x, int y) {
+		return grid[x][y]; 
+	}
+
+};
+
+Wall W1; // the right wall 
+Wall W2; // the floor ;
+Wall W3; // the left wall 
+Wall W5;  // the ceiling 
+Wall W4; 
+
+int score = 0; 
+float previousScore = 0; 
 double MovHor = 0; 
 double MovVer = 0; 
 bool shot = false;
@@ -12,19 +65,49 @@ bool shot = false;
 int a = 0;
 int b = 0; 
 int c = 0; 
-float t = 0; 
 
 float X0 = ROOM_SIZE;
-float Y0 = 2;
+float Y0 = 0.25*ROOM_SIZE;
 float Z0 = ROOM_SIZE / 2;
 
 float XF = 0;
 float YF = ROOM_SIZE/2 ;
 float ZF = ROOM_SIZE / 2;
  
-float YDrawn = Y0 + t * b; 
-float ZDrawn = Z0 + t * c; 
 
+float step = 0.003;
+float stepX = step * a;
+float stepY = step * b;
+float stepZ = step * c;
+
+
+float stepReplay = 0.0005;
+float XReplay = 0; 
+float YReplay = 0; 
+float ZReplay = 0; 
+bool hasBeenShot = false;
+bool replay = false; 
+
+int times = 0;
+
+
+int mode = 0; 
+void print(int x, int y, int z ,char* str)
+{
+	int len, i;
+	glColor3f(0,0,0); 
+	//set the position of the text in the window using the x and y coordinates
+	glRasterPos3f(x, y,z);
+
+	//get the length of the string to display
+	len = (int)strlen(str);
+
+	//loop to display character by character
+	for (i = 0; i < len; i++)
+	{
+		glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, str[i]);
+	}
+}
 void drawCircle(double cx, double cy, double cz, double r, int acc, int plane) { // 0 - >XY , 1 ->XZ  , 2 ->ZY
 
 
@@ -69,13 +152,13 @@ void axis(double length) { // draw a z-axis, with cone at end
 
 	
 }
-void drawRoom() {
-
-	glColor3f(0.0f, 0.0f, 0.0f);
-	glTranslatef(ROOM_SIZE /2, ROOM_SIZE /2, ROOM_SIZE /2);
-	glutWireCube(ROOM_SIZE);
-
-}
+//void drawRoom() {
+//
+//	glColor3f(0.0f, 0.0f, 0.0f);
+//	glTranslatef(ROOM_SIZE /2, ROOM_SIZE /2, ROOM_SIZE /2);
+//	glutWireCube(ROOM_SIZE);
+//
+//}
 
 void drawBrick(int x , int y , int z, int whichPlane) { // 0 ->XY , 1 ->XZ 
 		
@@ -101,81 +184,47 @@ void drawBrick(int x , int y , int z, int whichPlane) { // 0 ->XY , 1 ->XZ
 
 
 void drawWalls() {
-
-	float r1 = 210 / 255.0;
-	float g1 = 105 / 255.0;
-	float b1 = 30 / 255.0;
-
-	float r2 = 205 / 255.0;
-	float g2 = 133 / 255.0;
-	float b2 = 63 / 255.0;
-
-	float r3 = 188 / 255.0;
-	float g3 = 143 / 255.0;
-	float b3 = 143 / 255.0;
-
-	float r4 = 244 / 255.0;
-	float g4 = 164 / 255.0;
-	float b4 = 96 / 255.0;
-
-	float r5 = 139 / 255.0;
-	float g5 = 69 / 255.0;
-	float b5 = 19 / 255.0;
-
-	float r6 = 255 / 255.0;
-	float g6 = 228 / 255.0;
-	float b6 = 181 / 255.0;
-
-	float r7 = 255 / 255.0;
-	float g7 = 228 / 255.0;
-	float b7 = 225 / 255.0;
-
+	Color c1(79, 159, 108); //score 30 ; //dark Green
+	Color c2(125, 158, 137); // 20 //baby green
+	Color c3(229, 158, 58); // 5 ;  // pale yellow 
+	Color c4(255, 98, 58); // -5  // baby red
+	Color c5(255, 145, 0); // 1 // orange
+	Color c6(255, 25, 0); // -15 // red
+	Color c7(36, 97, 187); // 35 // blue 
 	//The Right Wall ; 
 	int cc = 0;
 	for (int  i = 0; i < ROOM_SIZE; i += BRICK_SIZE) {
 		for (int  j = 0; j < ROOM_SIZE; j += BRICK_SIZE) {
 			int color =cc%7; 
-			float r = 0; 
-			float g = 0; 
-			float b = 0; 
-			
 			if (color == 0) {
-				 r = r1;
-				 g = g1;
-				 b = b1;
+				glColor3f(c1.getRed(), c1.getGreen(), c1.getBlue());
+				W1.setCell(i/BRICK_SIZE,j/BRICK_SIZE,30); 
 			}
 			else if (color == 1) {
-				 r = r2;
-				 g = g2;
-				 b = b2;
+				glColor3f(c2.getRed(), c2.getGreen(), c2.getBlue());
+				W1.setCell(i/BRICK_SIZE, j / BRICK_SIZE, 20);
 			}
 			else if (color == 2) {
-				 r = r3;
-				 g = g3;
-				 b = b3;
+				glColor3f(c3.getRed(), c3.getGreen(), c3.getBlue());
+				W1.setCell(i / BRICK_SIZE, j / BRICK_SIZE, 5);
 			}
 			else if (color == 3) {
-				 r = r4;
-				 g = g4;
-				 b = b4;
+				glColor3f(c4.getRed(), c4.getGreen(), c4.getBlue());
+				W1.setCell(i / BRICK_SIZE, j / BRICK_SIZE, -5);
 			}
 			else if (color == 4) {
-				 r = r5;
-				 g = g5;
-				 b = b5;
+				glColor3f(c5.getRed(), c5.getGreen(), c5.getBlue());
+				W1.setCell(i / BRICK_SIZE, j / BRICK_SIZE, 1);
 			}
 			else if (color == 5) {
-				 r = r6;
-				 g = g6;
-				 b = b6;
+				glColor3f(c6.getRed(), c6.getGreen(), c6.getBlue());
+				W1.setCell(i / BRICK_SIZE, j / BRICK_SIZE, -15);
 			}
 			else  {
-				 r = r7;
-				 g = g7;
-				 b = b7;
+				glColor3f(c7.getRed(), c7.getGreen(), c7.getBlue());
+				W1.setCell(i / BRICK_SIZE, j / BRICK_SIZE, 35);
 			}
 			cc++; 
-			glColor3f(r, g, b);
 			drawBrick(i, j, 0 , 0 ); 
 		}
 	}
@@ -183,143 +232,108 @@ void drawWalls() {
 	for (int i = 0; i < ROOM_SIZE; i += BRICK_SIZE) {
 		for (int j = 0; j < ROOM_SIZE; j += BRICK_SIZE) {
 			int color = cc % 7;
-			float r = 0;
-			float g = 0;
-			float b = 0;
-
 			if (color == 0) {
-				r = r1;
-				g = g1;
-				b = b1;
+				glColor3f(c1.getRed(), c1.getGreen(), c1.getBlue());
+				W3.setCell(i / BRICK_SIZE, j / BRICK_SIZE, 30);
 			}
 			else if (color == 1) {
-				r = r2;
-				g = g2;
-				b = b2;
+				glColor3f(c2.getRed(), c2.getGreen(), c2.getBlue());
+				W3.setCell(i / BRICK_SIZE, j / BRICK_SIZE, 20);
 			}
 			else if (color == 2) {
-				r = r3;
-				g = g3;
-				b = b3;
+				glColor3f(c3.getRed(), c3.getGreen(), c3.getBlue());
+				W3.setCell(i / BRICK_SIZE, j / BRICK_SIZE, 5);
 			}
 			else if (color == 3) {
-				r = r4;
-				g = g4;
-				b = b4;
+				glColor3f(c4.getRed(), c4.getGreen(), c4.getBlue());
+				W3.setCell(i / BRICK_SIZE, j / BRICK_SIZE, -5);
 			}
 			else if (color == 4) {
-				r = r5;
-				g = g5;
-				b = b5;
+				glColor3f(c5.getRed(), c5.getGreen(), c5.getBlue());
+				W3.setCell(i / BRICK_SIZE, j / BRICK_SIZE, 1);
 			}
 			else if (color == 5) {
-				r = r6;
-				g = g6;
-				b = b6;
+				glColor3f(c6.getRed(), c6.getGreen(), c6.getBlue());
+				W3.setCell(i / BRICK_SIZE, j / BRICK_SIZE, -15);
 			}
 			else {
-				r = r7;
-				g = g7;
-				b = b7;
+				glColor3f(c7.getRed(), c7.getGreen(), c7.getBlue());
+				W3.setCell(i / BRICK_SIZE, j / BRICK_SIZE, 35);
 			}
 			cc++;
-			glColor3f(r, g, b);
 			drawBrick(i, j, ROOM_SIZE, 0);
 		}
 	}
-	// The Lower Wall 
+	// The floor 
 	for (int i = 0; i < ROOM_SIZE; i += BRICK_SIZE) {
 		for (int j = 0; j < ROOM_SIZE; j += BRICK_SIZE) {
 			int color = cc % 7;
-			float r = 0;
-			float g = 0;
-			float b = 0;
-
 			if (color == 0) {
-				r = r1;
-				g = g1;
-				b = b1;
+				glColor3f(c1.getRed(), c1.getGreen(), c1.getBlue());
+				W2.setCell(i / BRICK_SIZE, j / BRICK_SIZE, 30);
 			}
 			else if (color == 1) {
-				r = r2;
-				g = g2;
-				b = b2;
+				glColor3f(c2.getRed(), c2.getGreen(), c2.getBlue());
+				W2.setCell(i / BRICK_SIZE, j / BRICK_SIZE, 20);
 			}
 			else if (color == 2) {
-				r = r3;
-				g = g3;
-				b = b3;
+				glColor3f(c3.getRed(), c3.getGreen(), c3.getBlue());
+				W2.setCell(i / BRICK_SIZE, j / BRICK_SIZE, 5);
 			}
 			else if (color == 3) {
-				r = r4;
-				g = g4;
-				b = b4;
+				glColor3f(c4.getRed(), c4.getGreen(), c4.getBlue());
+				W2.setCell(i / BRICK_SIZE, j / BRICK_SIZE, -5);
 			}
 			else if (color == 4) {
-				r = r5;
-				g = g5;
-				b = b5;
+				glColor3f(c5.getRed(), c5.getGreen(), c5.getBlue());
+				W2.setCell(i / BRICK_SIZE, j / BRICK_SIZE, 1);
 			}
 			else if (color == 5) {
-				r = r6;
-				g = g6;
-				b = b6;
+				glColor3f(c6.getRed(), c6.getGreen(), c6.getBlue());
+				W2.setCell(i / BRICK_SIZE, j / BRICK_SIZE, -15);
 			}
 			else {
-				r = r7;
-				g = g7;
-				b = b7;
+				glColor3f(c7.getRed(), c7.getGreen(), c7.getBlue());
+				W2.setCell(i / BRICK_SIZE, j / BRICK_SIZE, 35);
 			}
 			cc++;
-			glColor3f(r, g, b);
 			drawBrick(i, 0, j, 1);
 		}
 	}
-	// The upper Wall 
+
+	// The  ceiling  
 	for (int i = 0; i < ROOM_SIZE; i += BRICK_SIZE) {
 		for (int j = 0; j < ROOM_SIZE; j += BRICK_SIZE) {
 			int color = cc % 7;
-			float r = 0;
-			float g = 0;
-			float b = 0;
-
 			if (color == 0) {
-				r = r1;
-				g = g1;
-				b = b1;
+				glColor3f(c1.getRed(), c1.getGreen(), c1.getBlue());
+				W5.setCell(i / BRICK_SIZE, j / BRICK_SIZE, 30);
 			}
 			else if (color == 1) {
-				r = r2;
-				g = g2;
-				b = b2;
+				glColor3f(c2.getRed(), c2.getGreen(), c2.getBlue());
+				W5.setCell(i / BRICK_SIZE, j / BRICK_SIZE, 20);
 			}
 			else if (color == 2) {
-				r = r3;
-				g = g3;
-				b = b3;
+				glColor3f(c3.getRed(), c3.getGreen(), c3.getBlue());
+				W5.setCell(i / BRICK_SIZE, j / BRICK_SIZE, 5);
 			}
 			else if (color == 3) {
-				r = r4;
-				g = g4;
-				b = b4;
+				glColor3f(c4.getRed(), c4.getGreen(), c4.getBlue());
+				W5.setCell(i / BRICK_SIZE, j / BRICK_SIZE, -5);
 			}
 			else if (color == 4) {
-				r = r5;
-				g = g5;
-				b = b5;
+				glColor3f(c5.getRed(), c5.getGreen(), c5.getBlue());
+				W5.setCell(i / BRICK_SIZE, j / BRICK_SIZE, 1);
 			}
 			else if (color == 5) {
-				r = r6;
-				g = g6;
-				b = b6;
+				glColor3f(c6.getRed(), c6.getGreen(), c6.getBlue());
+				W5.setCell(i / BRICK_SIZE, j / BRICK_SIZE, -15);
 			}
 			else {
-				r = r7;
-				g = g7;
-				b = b7;
+				glColor3f(c7.getRed(), c7.getGreen(), c7.getBlue());
+				W5.setCell(i / BRICK_SIZE, j / BRICK_SIZE, 35);
 			}
 			cc++;
-			glColor3f(r, g, b);
 			drawBrick(i, ROOM_SIZE, j, 1);
 
 		}
@@ -328,7 +342,7 @@ void drawWalls() {
 void drawShootingStation() {
 
 	glColor3f(0.5, 0.5, 0.5);
-	glTranslatef(ROOM_SIZE + 5, ROOM_SIZE*0.3, ROOM_SIZE / 2);
+	glTranslatef(1.5*ROOM_SIZE , ROOM_SIZE*0.3, ROOM_SIZE / 2);
 	glRotatef(90+MovHor, 0, 1, 0);
 	glRotatef(MovVer, 1, 0, 0);
 
@@ -336,101 +350,181 @@ void drawShootingStation() {
 	glutWireSphere(5, 20, 20);
 }
 
-//
-//void drawLaserPoint() {
-//	if ()
-//	drawCircle(); 
-//}
+
 void drawMissile() {
 
-	glColor3f(0.5, 0.5, 0.5);
 
-	glTranslatef(X0 + t*a, YDrawn, ZDrawn);
+	glutSolidSphere(0.5, 20, 20);
+	
+}
+void showingScore(int x=10 , int y=10 , int z =10) {
 
-	glutSolidSphere(1, 20, 20);
+	
+	char* result[20];
+	sprintf((char*)result, "Score :%d ", score);
+	print(x, y, z, (char*)result);
+}
+void updateScore(float x, float y, float z, int wall) { // wall 1 ->Right  , 2 ->floor , 3 ->Left , 4->Ceil  
+
+	//int numberOfBricks = ROOM_SIZE / BRICK_SIZE; 
+	int indeX = ceil(ceil(x) / BRICK_SIZE) - 1;
+	int indeY = ceil(ceil(y) / BRICK_SIZE) - 1;
+	int indeZ = ceil(ceil(z) / BRICK_SIZE) - 1;
+	if (wall == 1) {
+		score += W1.getCellPoints(indeX, indeY);
+		printf("W1 -> %d , %d\n", indeX, indeY);
+	}
+	if (wall == 3) {
+		score += W3.getCellPoints(indeX, indeY);
+		printf("W3 -> %d , %d\n", indeX, indeY);
+
+	}
+	if (wall == 2) {
+		score += W2.getCellPoints(indeX, indeZ);
+		printf("W2 -> %d , %d\n", indeX, indeZ);
+
+
+	}
+	if (wall == 4) {
+		score += W5.getCellPoints(indeX, indeZ);
+		printf("W5 -> %d , %d\n", indeX, indeZ);
+
+	}
+
+}
+void setupCamera() {
+	
+	if (mode == 0) {
+		glMatrixMode(GL_MODELVIEW);
+		glLoadIdentity();
+		gluLookAt(ROOM_SIZE * 2.3, ROOM_SIZE * 0.5, ROOM_SIZE / 2, 0.0f, ROOM_SIZE / 2, ROOM_SIZE / 2, 0.0f, 1.0f, 0.0f);
+	}
+	else if (mode == 1) { //simple
+		glMatrixMode(GL_MODELVIEW);
+		glLoadIdentity();
+		gluLookAt(ROOM_SIZE * 1.3+X0, ROOM_SIZE * 0.5, ROOM_SIZE / 2, 0.0f, ROOM_SIZE / 2, ROOM_SIZE / 2, 0.0f, 1.0f, 0.0f);
+	}
+	else {
+		glMatrixMode(GL_MODELVIEW);
+		glLoadIdentity();
+		gluLookAt(ROOM_SIZE * 2.0 + X0 - 0.7*ROOM_SIZE, ROOM_SIZE * 0.4, ROOM_SIZE *0.5, X0, Y0, Z0, 0.0f, 1.0f, 0.0f);
+	}
 	
 }
 void Display(void) {
+	setupCamera();
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 
-	glPushMatrix(); 
-	drawRoom(); 
-	glPopMatrix(); 
+	if (times >= 3) {
+		
+		if (replay) {
+			glPushMatrix();
+			drawWalls();
+			glPopMatrix();
+
+			glPushMatrix();
+			glColor3f(0, 0, 0);
+			glTranslatef(X0, Y0, Z0);
+			drawMissile();
+			glPopMatrix();
+		}
+		else {
+			showingScore(11,5,5);
+		}
+	}
+	else {
 
 
-	glPushMatrix();
-	//drawWalls();
-	glPopMatrix();
-	
-
-
-	glPushMatrix();
-	drawShootingStation(); 
-	glPopMatrix();
-
-
-	if (shot) {
 		glPushMatrix();
-		drawMissile();
+		drawWalls();
+		glPopMatrix();
+
+
+
+		if (!replay) {
+			glPushMatrix();
+			drawShootingStation();
+			glPopMatrix();
+		}
+
+
+		if (shot || replay) {
+			glPushMatrix();
+			glColor3f(0, 0, 0);
+			glTranslatef(X0, Y0, Z0);
+			drawMissile();
+			glPopMatrix();
+		}
+
+		showingScore(10,10,10);
+
+		glPushMatrix();
+		glColor3d(0, 1, 0); // draw black lines       
+		axis(20);      // z-axis 
+		glPopMatrix();
+
+
+
+		glPushMatrix();
+		glColor3d(0, 0, 1); // x-axis     
+		glRotatef(90, 0, 1, 0);
+		axis(20);
+		glPopMatrix();
+
+		glPushMatrix();
+		glColor3d(1, 0, 0); // y-axis     
+		glRotatef(-90, 1, 0, 0);
+		axis(20);
 		glPopMatrix();
 	}
-
-
-
-
-
-
-	glPushMatrix();
-	glColor3d(0, 1, 0); // draw black lines       
-	axis(20);      // z-axis 
-	glPopMatrix();
-
-
-
-	glPushMatrix();
-	glColor3d(0, 0, 1); // x-axis     
-	glRotatef(90, 0, 1, 0);
-	axis(20);
-	glPopMatrix();
-
-	glPushMatrix();
-	glColor3d(1, 0, 0); // y-axis     
-	glRotatef(-90, 1, 0, 0);
-	axis(20);
-	glPopMatrix();
 	glFlush();
 }
 
+
+
 void Anim() {
-	if (shot)
-		t+=0.005;
-	else
-		t = 0; 
-
-
-	if ( Y0 + t * b < 0)
-		YDrawn  = -(Y0+t*b); 
-	else if (Y0 + t * b > ROOM_SIZE) {
-		YDrawn = -((Y0 + t * b) - ROOM_SIZE) + ROOM_SIZE;
+	if (shot||replay) {
+		X0 += stepX; 
+		Y0 += stepY	;
+		Z0 += stepZ; 
 	}
-	else {
-		YDrawn = (Y0 + t * b);
+	if (Y0 > ROOM_SIZE || Y0 < 0) {
+		if (shot) {
+			if (Y0 > ROOM_SIZE) {
+				updateScore(X0, ROOM_SIZE, Z0, 4);
+			}
+			else {
+				updateScore(X0, 0, Z0, 2);
+
+			}
+		}
+		printf("%d\n", score);
+		stepY = -stepY; 
+	}
+	if (Z0 > ROOM_SIZE || Z0 < 0) {
+		if (shot) {
+			if (Z0 > ROOM_SIZE) {
+				updateScore(X0, Y0, ROOM_SIZE, 3);
+			}
+			else {
+				updateScore(X0, Y0, 0, 1);
+
+			}
+		}
+		printf("%d\n", score);
+		stepZ = -stepZ;
 	}
 
-
-	if (Z0 + t * c < 0)
-		ZDrawn = -(Z0+t*c);
-	else if (Z0 + t * c > ROOM_SIZE) {
-		ZDrawn = -((Z0 + t * c)-ROOM_SIZE) + ROOM_SIZE;
-	}
-	else {
-		ZDrawn = (Z0 + t * c);
-	}
-
-
-	if (X0 + t * a <= 0) {
+	if (X0  <= 0) {
+		if (shot)
+		//times++;
 		shot = false;
+		replay = false;
+		X0 = ROOM_SIZE; 
+		Y0 = ROOM_SIZE*0.25; 
+		Z0 = ROOM_SIZE / 2;
+		hasBeenShot = true; 
 	}
 
 	glutPostRedisplay();
@@ -440,30 +534,63 @@ void Anim() {
 
 void Controllers(unsigned char k, int x, int y)
 {
-	if (k == 'd') {
-		MovHor -= 1;
-		ZF--; 
-	}
-	if (k == 'a') {
-		MovHor += 1;
-		ZF++; 
-	}
-	if (k == 'w') {
-		MovVer += 1;
-		YF++; 
-	}
-	if (k == 's') {
-		MovVer -= 1;
-		YF--; 
-	}
-	a = XF - X0; 
-	b = YF - Y0; 
-	c = ZF - Z0; 
 
-	YDrawn = Y0 + t * b;
-	ZDrawn = Z0 + t * c;
-	if (k == ' ')
-		shot = true;
+	if (k == 'h') {
+		mode = 0;
+	}
+	if (k == 'j') {
+		mode = 1;
+	}
+	if (k == 'k') {
+		mode = 2;
+	}
+		if (!shot && !replay) {
+			if (k == 'r' && hasBeenShot) {
+				a = XReplay - X0;
+				b = YReplay - Y0;
+				c = ZReplay - Z0;
+				stepX = stepReplay * a;
+				stepY = stepReplay * b;
+				stepZ = stepReplay * c;
+				replay = true;
+			}
+	 
+			else if (times < 3) {
+				if (k == 'd') {
+					MovHor -= 1;
+					ZF--;
+				}
+				if (k == 'a') {
+					MovHor += 1;
+					ZF++;
+				}
+				if (k == 'w') {
+					MovVer += 1;
+					YF++;
+				}
+				if (k == 's') {
+					MovVer -= 1;
+					YF--;
+				}
+
+				/*printf("%f\n", YF); */
+				a = XF - X0;
+				b = YF - Y0;
+				c = ZF - Z0;
+				stepX = step * a;
+				stepY = step * b;
+				stepZ = step * c;
+
+				if (k == ' ') {
+					shot = true;
+					XReplay = XF;
+					YReplay = YF;
+					ZReplay = ZF;
+
+				}
+			
+		}
+	}
 
 }
 
@@ -474,6 +601,8 @@ void main(int argc, char** argv) {
 	glutInitWindowPosition(150, 150);
 
 	glutCreateWindow("OpenGL - 3D Template");
+
+
 	glutDisplayFunc(Display);
 	glutIdleFunc(Anim);
 	glutKeyboardFunc(Controllers);
@@ -487,9 +616,9 @@ void main(int argc, char** argv) {
 	glLoadIdentity();
 	gluPerspective(60.0f, 16 / 9, 0.1f, 300.0f);
 
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-	gluLookAt(18.0f, ROOM_SIZE*0.5, ROOM_SIZE / 2, 0.0f, ROOM_SIZE / 2, ROOM_SIZE / 2, 0.0f, 1.0f, 0.0f);
+
 
 	glutMainLoop();
 }
+
+
